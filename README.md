754762aa1e84db3a90a50417aa0ecbe59b661ba0
Dennis Kibet <dennisrkibet@gmail.com>
2024-11-08 15:26:13 +0300

    setting up configurations

---
+++ README.md
@@ -0,0 +1,1 @@
+# FashionYuva Backend
--- config.py
+++ config.py
@@ -6,4 +6,6 @@ load_dotenv()
 
 
 class Config:
+    SQLALCHEMY_DATABASE_URI = "postgresql://dennis:dennis@localhost/fashionyuva"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv("SECRET_KEY", "46ergsgwet5w5fewfweffhdgh")

dbdae71a2cfe9b14c6deb3c1cd5824838e07b134
Kigo-P <peterkigo33@gmail.com>
2024-11-08 21:51:11 +0300

    created models

--- models.py
+++ models.py
@@ -31,10 +31,18 @@ class User(db.Model, SerializerMixin):
     orders = db.relationship(
         "Order", back_populates="user", cascade="all, delete-orphan"
     )
+    # a relationship that maps the user to the carts
+    carts = db.relationship("Cart", back_populates="user", cascade="all, delete-orphan")
+    # a relationship that maps the user to the carts
+    reviews = db.relationship("Review", back_populates="user", cascade="all, delete-orphan")
+    # a relationship that maps the user to the address
+    address = db.relationship("Address", back_populates="user", cascade="all, delete-orphan")
 
     serialize_rules = (
         "-orders.user",
         "-password",
+        "-carts.user",
+        "-address.user",
     )
 
     #  creating a string version using repr
@@ -69,11 +77,20 @@ class Product(db.Model, SerializerMixin):
     orders = db.relationship(
         "Order", back_populates="product", cascade="all, delete-orphan"
     )
+    # a relationship that maps the product to the carts
+    carts = db.relationship(
+        "Cart", back_populates="product", cascade="all, delete-orphan"
+    )
+    # a relationship that maps the product to the reviews
+    reviews = db.relationship(
+        "Review", back_populates="product", cascade="all, delete-orphan"
+    )
 
     serialize_rules = (
         "-images.product",
         "-categories.product",
         "-orders.product",
+        "-reviews.product",
     )
 
     #  creating a string version using repr
@@ -137,8 +154,91 @@ class Order(db.Model, SerializerMixin):
     user = db.relationship("User", back_populates="orders")
     # a relationship that maps the orders to the products
     product = db.relationship("Product", back_populates="orders")
+    
 
     serialize_rules = (
         "-product.orders",
         "-user.orders",
     )
+
+    #  creating a string version using repr
+    def __repr__(self):
+        return f"<Order {self.id}: {self.total_price} , {self.quantity}, {self.status}has been created>"
+
+# creating a model called Cart with the table name of carts
+class Cart(db.Model, SerializerMixin):
+    __tablename__ = "carts"
+
+    # creating columns
+    id = db.Column(db.Integer, primary_key=True)
+    total_price = db.Column(db.Integer(), nullable=False)
+    quantity = db.Column(db.Integer(), nullable=False)
+    status = db.Column(db.String(), nullable=False)
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    updated_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    # Foreign key from the user_id
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    # Foreign key from the product_id
+    product_id = db.Column(db.Integer, db.ForeignKey("products.id"))
+
+    # a relationship that maps the carts to the user
+    user = db.relationship("User", back_populates="carts")
+    # a relationship that maps the carts to the product
+    product = db.relationship("Product", back_populates="carts")
+
+    serialize_rules = (
+        "-product.carts",
+        "-user.carts",
+    )
+
+    #  creating a string version using repr
+    def __repr__(self):
+        return f"<Cart {self.id}: {self.total_price} , {self.quantity}, {self.status}has been created>"
+
+# creating a model called Review with the table name of reviews
+class Review(db.Model, SerializerMixin):
+    __tablename__ = "reviews"
+
+    # creating columns
+    id = db.Column(db.Integer, primary_key=True)
+    rating= db.Column(db.String(), nullable=False)
+    comment = db.Column(db.String(), nullable=False)
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    # Foreign key from the user_id
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    # Foreign key from the product_id
+    product_id = db.Column(db.Integer, db.ForeignKey("products.id"))
+
+    # a relationship that maps the carts to the user
+    user = db.relationship("User", back_populates="reviews")
+    # a relationship that maps the carts to the product
+    product = db.relationship("Product", back_populates="reviews")
+
+    #  creating a string version using repr
+    def __repr__(self):
+        return f"<Review {self.id}: {self.rating} , {self.comment} has been created>"
+    
+# creating a model called Address with the table name of addresses
+class Address(db.Model, SerializerMixin):
+    __tablename__ = "addresses"
+
+    # creating columns
+    id = db.Column(db.Integer, primary_key=True)
+    address= db.Column(db.String(), nullable=False)
+    county= db.Column(db.String(), nullable=False)
+    town= db.Column(db.String(), nullable=False)
+    zip_code = db.Column(db.Integer(), nullable=False)
+    country= db.Column(db.String(), nullable=False)
+    # Foreign key from the user_id
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+    # a relationship that maps the address to the user
+    user = db.relationship("User", back_populates="address")
+
+    serialize_rules = (
+        "-user.address",
+    )
+
+    #  creating a string version using repr
+    def __repr__(self):
+        return f"<Address {self.id}: {self.address} , {self.county} {self.town} {self.country} has been created>"

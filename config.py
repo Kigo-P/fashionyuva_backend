698171fe1d5e32a81ebb60f34a51d3033a768207
Dennis Kibet <dennisrkibet@gmail.com>
2024-11-08 15:51:03 +0300

    added env variables for postgres URL STRING

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ load_dotenv()
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql://dennis:dennis@localhost/fashionyuva"
+    SQLALCHEMY_DATABASE_URI = f"postgresql://{os.getenv("POSTGRES_USERNAME")}:{os.getenv("POSTGRES_PASSWORD")}@localhost/fashionyuva"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv("SECRET_KEY", "46ergsgwet5w5fewfweffhdgh")

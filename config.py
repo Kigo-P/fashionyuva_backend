8c4d3e065c7f19b9b52cc6cc7c8a0622010888e8
Dennis Kibet <dennisrkibet@gmail.com>
2024-11-10 14:14:16 +0300

    development and production URLS

--- config.py
+++ config.py
@@ -6,7 +6,10 @@ load_dotenv()
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = f"postgresql://{os.getenv('POSTGRES_USERNAME')}:{os.getenv('POSTGRES_PASSWORD')}@localhost/fashionyuva"
-    ## SQLALCHEMY_DATABASE_URI = f"postgresql://dennis:TTOszQYBlz7ljFPdZhQtlFvd3PucGvwi@dpg-cso8dj8gph6c73bo21vg-a/fashionyuva"
+    ENV = os.getenv("ENV", "development")
+    if ENV == "production":
+        SQLALCHEMY_DATABASE_URI = f"postgresql://dennis:TTOszQYBlz7ljFPdZhQtlFvd3PucGvwi@dpg-cso8dj8gph6c73bo21vg-a/fashionyuva"
+    else:
+        SQLALCHEMY_DATABASE_URI = f"postgresql://{os.getenv('POSTGRES_USERNAME')}:{os.getenv('POSTGRES_PASSWORD')}@localhost/fashionyuva"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv("SECRET_KEY", "46ergsgwet5w5fewfweffhdgh")
